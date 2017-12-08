168d26f1a1fc385bded4c501f4146002509a6bf0
Roth <rothdyt@gmail.com>
2017-12-07 22:15:30 -0600

    categorical added

--- PROJECT_HOUSING_PRICES/Shinyapp/app.R
+++ PROJECT_HOUSING_PRICES/Shinyapp/app.R
@@ -5,6 +5,8 @@
 library(shiny)
 library(shinydashboard)
 library(leaflet)
+library(dplyr)
+library(ggplot2)
 
 # r_colors <- rgb(t(col2rgb(colors()) / 255))
 # names(r_colors) <- colors()
@@ -13,6 +15,7 @@ library(leaflet)
 load("fulldataset.Rdata")
 popupcontents <- paste("room type:",as.character(fulldataset$room_type),"address:",
                        as.character(fulldataset$street),sep="|")
+neighbors <- levels(fulldataset$neighbourhood_cleansed)
 
 houseIcons <- icons(
   iconUrl = ifelse(fulldataset$median_price<=200,"./icons/green.png",
@@ -20,20 +23,23 @@ houseIcons <- icons(
   iconWidth = 10, iconHeight = 10
 )
 
+# ui
 ui <- dashboardPage(
   dashboardHeader(title = "Graphs"),
   dashboardSidebar(
     sidebarMenu(
       menuItem(text="Map", tabName = "Map"),
-      menuItem(text="EDA", tabName = "EDA")
+      menuItem(text="Numerical Variabels", tabName = "num"),
+      menuItem(text="Categorical Variabels", tabName = "cat"),
+      menuItem(text="Reviews", tabName="review")
     )
   ),
   dashboardBody(
     tabItems(
-      # First tab content
+      # Map tab content
       tabItem(tabName = "Map",
               h2("Airbnb Rental Markets in Boston"),
-              p("Choose your map type to display:"),
+              p("Choose a map to display:"),
               actionButton(inputId="house", label="Apt/Room"),
               actionButton(inputId="neighborhood", label="Neighborhoods"),
               actionButton(inputId="host", label="SuperHost"),
@@ -41,10 +47,10 @@ ui <- dashboardPage(
               leafletOutput("map", height = 600)
       ),
       
-      # Second tab content
+      # Numerical tab content
       tabItem(
-        tabName = "EDA",
-        h2("Explore Data Analysis"),
+        tabName = "num",
+        h2("Explore Data Analysis - Numerical Variabels"),
         fluidRow(
           box(plotOutput("plotbyhouse", height = "400px")),
           
@@ -53,7 +59,73 @@ ui <- dashboardPage(
             sliderInput("slider", "Number of observations:", 1, 100, 50)
           )
         )
+      ),
+      
+      # Categorical tab content
+      tabItem(
+        tabName = "cat",
+        h2("Explore Data Analysis - Categorical Variabels"),
+        fluidRow(
+          tabBox(
+            title = "Choose Variables",
+            id = "cat_var", height = "500px",width = "100%",
+            tabPanel(title="Neighborhood", plotOutput("neighborhood_price_boxplot")),
+            tabPanel(title="# Bedrooms", plotOutput("neighborhood_bedroom_dotplot"))
+          )
+        ),
+       fluidRow(
+         box(
+           title = "Controls",
+           width = "100%",
+           checkboxGroupInput(
+             inputId = "Neighborhood_checkbox", "neighborhood to show:",
+             # code to generate choices
+             # neighbors <- levels(fulldataset$neighbourhood_cleansed)
+             # for (i in 1:length(neighbors)){
+             #   if (i != length(neighbors)){
+             #     cat('"',neighbors[i],'"',"=",
+             #         '"',neighbors[i],'"',",","\n",
+             #         sep="") 
+             #   }else{
+             #     cat('"',neighbors[i],'"',"=",
+             #         '"',neighbors[i],'"',"\n",
+             #         sep="")
+             #   }
+             # }
+             choices= c("Allston"="Allston",
+                        "Back Bay"="Back Bay",
+                        "Bay Village"="Bay Village",
+                        "Beacon Hill"="Beacon Hill",
+                        "Brighton"="Brighton",
+                        "Charlestown"="Charlestown",
+                        "Chinatown"="Chinatown",
+                        "Dorchester"="Dorchester",
+                        "Downtown"="Downtown",
+                        "East Boston"="East Boston",
+                        "Fenway"="Fenway",
+                        "Hyde Park"="Hyde Park",
+                        "Jamaica Plain"="Jamaica Plain",
+                        "Leather District"="Leather District",
+                        "Longwood Medical Area"="Longwood Medical Area",
+                        "Mattapan"="Mattapan",
+                        "Mission Hill"="Mission Hill",
+                        "North End"="North End",
+                        "Roslindale"="Roslindale",
+                        "Roxbury"="Roxbury",
+                        "South Boston"="South Boston",
+                        "South Boston Waterfront"="South Boston Waterfront",
+                        "South End"="South End",
+                        "West End"="West End",
+                        "West Roxbury"="West Roxbury"),
+             selected = c("Allston"="Allston",
+                          "Back Bay"="Back Bay",
+                          "Bay Village"="Bay Village"),
+             inline = TRUE
+           )
+         )
+       )
       )
+      
     )
   )  
 )
@@ -138,7 +210,22 @@ Click dots will show their corresponding neighborhoods.")
     data <- histdata[seq_len(input$slider)]
     hist(data)
   })
+  selection <- reactive(input$Neighborhood_checkbox)
+  data <- reactive(filter(fulldataset, neighbourhood_cleansed %in% selection()))
+  my <- reactive(data.frame(price=data()$median_price,
+                   neighbourhood=data()$neighbourhood_cleansed,
+                   bedroom=data()$bedrooms))
+  ## content for boxplot price v.s neighbors
+  output$neighborhood_price_boxplot <- renderPlot({
+    ggplot(my(),aes(x=neighbourhood,y=price,colour=neighbourhood)) + geom_boxplot() +
+      theme(legend.position="none")
+  })
   
+  output$neighborhood_bedroom_dotplot <- renderPlot({
+    ggplot(my(),aes(x=neighbourhood,y=bedroom,colours, colour=neighbourhood)) +
+      geom_point(position=position_jitter(width=.1, height=0.1)) +
+      theme(legend.position="none")
+  })
   
 }
 
